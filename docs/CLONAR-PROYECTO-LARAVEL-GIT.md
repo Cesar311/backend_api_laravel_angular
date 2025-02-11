c8bf87f677491b7ecad2534521da97f82cbe6d1c
Cesar311 <cesar.aruquipa@gmail.com>
2025-02-11 13:39:43 -0400

    CRUD Usuarios

---
+++ docs/CLONAR-PROYECTO-LARAVEL-GIT.md
@@ -0,0 +1,22 @@
+## Clonar el Proyecto
+```
+git clone https://github.com/Cesar311/backend_api_laravel_angular.git
+```
+### Configuración
+- Abrir el Proyecto clonado
+- Copiar el archivo .env.exampley renombrar a .env
+### Instalar las dependencias del proyecto
+```
+composer update
+```
+### Generar app key
+```
+php artisan key:generate
+```
+
+## Configurar la base de datos en (.env)
+
+### Generar las migraciones
+```
+php artisan migrate
+```
--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,14 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UsuarioController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
 
 
 Route::prefix('/v1/auth')->group(function(){
@@ -21,3 +23,7 @@ Route::prefix('/v1/auth')->group(function(){
 
     });
 });
+
+
+// CRUD Usuarios
+Route::apiResource("usuario", UsuarioController::class);
